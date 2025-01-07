c6938955fb77aa0ea3e01cff1b1447563d5b65aa
san <sansivan.r@gmail.com>
2025-01-07 20:52:05 +0530

    trying

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
-import Login from './login';
+import Login from './login/login';
 import './App.css';
+import {Routes, Route, BrowserRouter} from 'react-router';
 
 function App() {
   return (
-    <>
-    <Login/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+      </Routes>
+    </BrowserRouter>
   );
 }
 
