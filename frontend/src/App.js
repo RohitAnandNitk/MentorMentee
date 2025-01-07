cfeb1c32bb30910d379d55890d51df425def0875
RohitAnandNitk <sharmarohit64748@gmail.com>
2025-01-07 20:39:31 +0530

    navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
-import Login from './login';
+
 import './App.css';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <>
-    <Login/>
+    {/* <Login/> */}
+    <Navbar/>
+    <HomePage/>
+    <Footer/>
     </>
   );
 }
