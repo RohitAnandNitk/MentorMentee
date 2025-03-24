ac5427491441a07fbe617ecef327cb34ae8f3c13
RohitAnandNitk <sharmarohit64748@gmail.com>
2025-03-24 20:33:00 +0530

    mentor profile added

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -3,9 +3,9 @@
 const config = {
   // Uncomment the one you want to use
 
-  BASE_URL: "http://localhost:4000", // Local development URL
+  // BASE_URL: "http://localhost:4000", // Local development URL
 
-  // BASE_URL: "https://mentormentee-so39.onrender.com", // Deployed production URL
+  BASE_URL: "https://mentormentee-so39.onrender.com", // Deployed production URL
 };
 
 export default config;
